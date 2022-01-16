a9bdfd5fc428b326d7d856fdf6677cc0705f8517
Christopher Chong <t.kleinchong@gmail.com>
2022-01-16 17:40:42 +0800

    Implement reading request body

--- main.go
+++ main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
-	"encoding/json"
+	"io/ioutil"
 	"fmt"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"encoding/json"
 )
+
+type BodyText struct {
+	Text string
+}
 
 func main() {
 	router := mux.NewRouter()
 	listeningPort := "55100"
-	listeningAddress := fmt.Sprintf("0.0.0.0:%s", listeningPort)
+	listeningAddress := fmt.Sprintf("localhost:%s", listeningPort)
 
-	router.HandleFunc("/summariser", Summarise).Methods(http.MethodGet)
+	router.HandleFunc("/summariser", summarise).Methods(http.MethodGet)
 	log.Println("Summariser waiting for content...")
 	http.ListenAndServe(listeningAddress, router)
 }
 
-func Summarise(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("Hello world!")
+func summarise(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	quit(err)
+
+	var textBody BodyText
+	err = json.Unmarshal(body, &textBody)
+	quit(err)
+	log.Println(textBody.Text)
+}
+
+func quit(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
