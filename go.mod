04d1b929b8cca850e588ad87351af087c96f0663
Christopher Chong <t.kleinchong@gmail.com>
2022-01-30 15:56:32 +0800

    Add logic to call gpt3 api and accept more body params

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
+	github.com/sashabaranov/go-gpt3 v0.0.0-20220110070523-eecd21296372
 )
--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
-	"io/ioutil"
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
-	"encoding/json"
-	"github.com/joho/godotenv"
 	"os"
+	"strings"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	gogpt "github.com/sashabaranov/go-gpt3"
 )
 
 type BodyText struct {
 	Text string
+	Temperature float32
+	Engine string
+	TopP float32
 }
+
+type JsonResponse struct {
+	Summary string
+	Text string
+	Temperature float32
+	Engine string
+	TopP float32
+}
+
+var MAX_WORD int = 1000
+var MAX_TOKEN int = 500
 
 func main() {
 	godotenv.Load()
@@ -21,31 +39,110 @@ func main() {
 	listeningPort := "55100"
 	listeningAddress := fmt.Sprintf("localhost:%s", listeningPort)
 
-	router.HandleFunc("/summariser", summarise).Methods(http.MethodGet)
+	router.HandleFunc("/summariser", summarise).Methods(http.MethodPost)
 	log.Println("Summariser waiting for content...")
 	http.ListenAndServe(listeningAddress, router)
 }
 
 func summarise(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+
 	body, err := ioutil.ReadAll(r.Body)
-	quit(err)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
 
 	var textBody BodyText
 	// passing in the memory address of textBody as json Unmarshal accepts pointer
 	err = json.Unmarshal(body, &textBody)
-	quit(err)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
 
-	getOpenAi(textBody.Text)
+	if textBody.Text == "" {
+		err = errors.New("invalid input body")
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+	text := trimText(textBody.Text)
+
+	validatedBody, err := validateTextBody(textBody)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	summarisedText, err := runGPT3(text, validatedBody)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	response := JsonResponse{
+		Summary: summarisedText,
+		Temperature: validatedBody.Temperature,
+		TopP: validatedBody.TopP,
+		Engine: validatedBody.Engine,
+		Text: text,
+	}
+	json.NewEncoder(w).Encode(response)
 }
 
-func getOpenAi(text string) {
+func validateTextBody(textBody BodyText) (BodyText, error) {
+	if textBody.Temperature < 0 || textBody.Temperature > 1 {
+		return textBody, errors.New("temperature set cannot be higher than 1 or lower than 0")
+	}
+	if textBody.TopP < 0 || textBody.TopP > 1 {
+		return textBody, errors.New("top_p set cannot be higher than 1 or lower than 0")
+	}
+	validEngines := map[string]bool {
+		"text-davinci-001": true,
+		"text-curie-001": true,
+		"text-babbage-001": true,
+		"text-ada-001": true,
+	}
+	if _, ok := validEngines[textBody.Engine]; !ok {
+		return textBody, errors.New("engine submitted is not valid, must be either text-davinci-001, text-curie-001, text-babbage-001 or text-ada-001")
+	}
+	return textBody, nil
+}
+
+func trimText(text string) string {
+	log.Printf("Trimming body text to be under %d words", MAX_WORD)
+	var trimmedWords []string
+	words := strings.Fields(text)
+	if len(words) > MAX_WORD {
+		trimmedWords = words[:MAX_WORD]	
+	} else {
+		trimmedWords = words
+	}
+	trimmedText := strings.Join(trimmedWords, " ")
+	log.Printf("Trimmed text: %s", trimmedText)
+	return trimmedText
+}
+
+func runGPT3(text string, validatedBody BodyText) (string, error) {
+	ctx := context.Background()
 	apiKey := os.Getenv("OPENAI_API_KEY")
 	text = text + "\ntl;dr:"
 	
-}
-
-func quit(err error) {
-	if err != nil {
-		log.Fatal(err)
+	client := gogpt.NewClient(apiKey)
+	req := gogpt.CompletionRequest{
+		Prompt: text,
+		MaxTokens: MAX_TOKEN,
+		Temperature: validatedBody.Temperature,
+		TopP: validatedBody.TopP,
 	}
+	resp, err := client.CreateCompletion(ctx, validatedBody.Engine, req)
+	if len(resp.Choices) == 0 {
+		return "", errors.New("API response error")
+	}
+	return resp.Choices[0].Text, err
 }
